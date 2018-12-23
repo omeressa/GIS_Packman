0621a4df431f821b274c50a638c2b49e9d8d06ae
admin <admin@DESKTOP-BUNN6F5>
2018-12-24 01:28:47 +0200

    perfecting

--- OOP_EX2-Ex4/src/Game/run.java
+++ OOP_EX2-Ex4/src/Game/run.java
@@ -5,11 +5,18 @@ import javax.swing.JFrame;
 import GUI.MyFrame;
 
 
+
 public class run {
-	public static void main(String[] args) {
+	
+	private static void run() {
+		// TODO Auto-generated method stub
 		MyFrame frame = new MyFrame();
 		frame.setVisible(true);
 		frame.setSize(frame.image.getWidth(),frame.image.getHeight());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 	}
+	public static void main(String[] args) {
+		run();
+	}
+
 }
